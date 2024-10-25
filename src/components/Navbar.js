534dadb261bbc094a5fa80bd0002f063c6807900
AdarshJha05 <adarshj595@gmail.com>
2024-10-25 19:21:57 +0530

    add Oauth2 bsed authentication

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,52 +1,98 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Navbar(props) {
-    return (
-        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-            <div className="container-fluid">
-                <NavLink className="navbar-brand" to="/">{props.title}</NavLink>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <NavLink
-                                className="nav-link"
-                                aria-current="page"
-                                to="/"
-                                end
-                            >
-                                Home
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink
-                                className="nav-link"
-                                to="/about"
-                            >
-                                {props.aboutText}
-                            </NavLink>
-                        </li>
-                    </ul>
-                    <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
-                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
-                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
-                    </div>
-                </div>
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+//   const {user } = useAuth0();
+  return (
+    <nav
+      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+    >
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">
+          {props.title}
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to="/" end>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">
+                {props.aboutText}
+              </NavLink>
+            </li>
+          </ul>
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
+            <input
+              className="form-check-input"
+              onClick={props.toggleMode}
+              type="checkbox"
+              id="flexSwitchCheckDefault"
+            />
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+            >
+              Enable DarkMode
+            </label>
+          </div>
+
+          {isAuthenticated ? (
+            <div className="mx-2">
+              <button
+                className="btn btn-primary"
+                onClick={() =>
+                  logout({ logoutParams: { returnTo: window.location.origin } })
+                }
+              >
+                Log Out
+              </button>
             </div>
-        </nav>
-    );
+          ) : (
+            <div className="mx-2">
+              <button
+                className="btn btn-primary"
+                onClick={() => loginWithRedirect()}
+              >
+                Log In
+              </button>
+            </div>
+          )}
+          {/* <div className="flex mx-3 align-items-center text-center">
+            {isAuthenticated && <p> {user.name}</p>}
+          </div> */}
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-    aboutText: PropTypes.string.isRequired
-}
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string.isRequired,
+};
 
 Navbar.defaultProps = {
-    title: 'Set title here',
-    aboutText: 'About'
+  title: "Set title here",
+  aboutText: "About",
 };
